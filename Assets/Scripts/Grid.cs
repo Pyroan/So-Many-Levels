302022b16b918808a3b564bdf12ec9945400e4b1
cdgira <cdgira@cs.ship.edu>
2017-09-01 23:37:38 -0400

    Initial Set of Classes for Auto Level Generation
    Still needs to be hooked into the main code that drives setting up the levels for the game.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -181,4 +181,230 @@ public class Grid : MonoBehaviour {
 	{
 		goalPosition = prevPosition;
 	}
+
+    /**
+     * Attempts to create a new random level to replace the just completed level.
+     */
+    int[,] buildNextMap(int[,] map1, Vector2 offset1, int[,] map2, Vector2 offset2)
+    {
+        int height = Random.Range(1, 7) + 5;
+        if (height % 2 == 0)
+            height++;
+        int width = height;
+        int[,] newMap = new int[height, width];
+
+        for (int row = 0; row < height; row++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                newMap[row, col] = -1;
+            }
+        }
+
+        // For the Player
+        newMap[height/2, width/2] = 0;
+
+        if (Random.Range(1,2) == 1) // East West Exit
+        {
+            newMap[height / 2, width / 2 + 1] = 0;
+            newMap[height / 2, width / 2 - 1] = 0;
+        }
+        else // North South Exit
+        {
+            newMap[height / 2 + 1, width / 2] = 0;
+            newMap[height / 2 - 1, width / 2] = 0;
+        }
+
+        int startRow = (int)(height/2 + offset1.y - map1.GetLength(1)/2);
+        int startCol = (int)(width/2 + offset1.x - map1.GetLength(0)/2);
+        int locX = 0;
+        for (int row = startRow; row < height; row++)
+        {
+            int locY = 0;
+            for (int col = startCol; col < width; col++)
+            {
+                if (row > 0 && col > 0)
+                {
+                    if (locX < map1.GetLength(0) && locY < map2.GetLength(1))
+                    {
+                        if (map1[locX, locY] == 1)
+                            newMap[row, col] = 0;
+                    }  
+                }
+                locY++;
+            }
+            locX++;
+        }
+
+        startRow = (int)(height / 2 + offset2.y - map2.GetLength(1) / 2);
+        startCol = (int)(width / 2 + offset2.x - map2.GetLength(0) / 2);
+        locX = 0;
+        for (int row = startRow; row < height; row++)
+        {
+            int locY = 0;
+            for (int col = startCol; col < width; col++)
+            {
+                if (row > 0 && col > 0)
+                {
+                    if (locX < map1.GetLength(0) && locY < map2.GetLength(1))
+                    {
+                        if (map1[locX, locY] == 1)
+                            newMap[row, col] = 0;
+                    }
+                }
+                locY++;
+            }
+            locX++;
+        }
+
+        // Need to slim down the walls to the new maze
+        for (int row = 0; row < height; row++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                if (newMap[row, col] == -1)
+                {
+                    int pathType = Random.Range(1, 6);
+                    switch (pathType)
+                    {
+                        case 1: // east west
+                            newMap[row - 1, col - 1] = swapValue(newMap[row - 1, col - 1],0);
+                            newMap[row - 1, col] = swapValue(newMap[row - 1, col],0);
+                            newMap[row - 1, col + 1] = swapValue(newMap[row - 1, col + 1],0);
+
+                            newMap[row, col - 1] = swapValue(newMap[row, col - 1], 1);
+                            newMap[row, col] = swapValue(newMap[row, col], 1);
+                            newMap[row, col + 1] = swapValue(newMap[row, col + 1], 1);
+
+                            newMap[row + 1, col - 1] = swapValue(newMap[row + 1, col - 1],0);
+                            newMap[row + 1, col] = swapValue(newMap[row + 1, col],0);
+                            newMap[row + 1, col + 1] = swapValue(newMap[row + 1, col + 1],0);
+                            break;
+                        case 2:  // top left
+                            newMap[row - 1, col - 1] = swapValue(newMap[row - 1, col - 1], 0);
+                            newMap[row - 1, col] = swapValue(newMap[row - 1, col], 0);
+                            newMap[row - 1, col + 1] = swapValue(newMap[row - 1, col + 1], 0);
+
+                            newMap[row, col - 1] = swapValue(newMap[row, col - 1], 0);
+                            newMap[row, col] = swapValue(newMap[row, col], 1);
+                            newMap[row, col + 1] = swapValue(newMap[row, col + 1], 1);
+
+                            newMap[row + 1, col - 1] = swapValue(newMap[row + 1, col - 1], 0);
+                            newMap[row + 1, col] = swapValue(newMap[row + 1, col], 1);
+                            newMap[row + 1, col + 1] = swapValue(newMap[row + 1, col + 1], 0);
+                            break;
+                        case 3: // top right
+                            newMap[row - 1, col - 1] = swapValue(newMap[row - 1, col - 1], 0);
+                            newMap[row - 1, col] = swapValue(newMap[row - 1, col], 0);
+                            newMap[row - 1, col + 1] = swapValue(newMap[row - 1, col + 1], 0);
+
+                            newMap[row, col - 1] = swapValue(newMap[row, col - 1], 1);
+                            newMap[row, col] = swapValue(newMap[row, col], 1);
+                            newMap[row, col + 1] = swapValue(newMap[row, col + 1], 0);
+
+                            newMap[row + 1, col - 1] = swapValue(newMap[row + 1, col - 1], 0);
+                            newMap[row + 1, col] = swapValue(newMap[row + 1, col], 1);
+                            newMap[row + 1, col + 1] = swapValue(newMap[row + 1, col + 1], 0);
+                            break;
+                        case 4: // bottom right
+                            newMap[row - 1, col - 1] = swapValue(newMap[row - 1, col - 1], 0);
+                            newMap[row - 1, col] = swapValue(newMap[row - 1, col], 1);
+                            newMap[row - 1, col + 1] = swapValue(newMap[row - 1, col + 1], 0);
+
+                            newMap[row, col - 1] = swapValue(newMap[row, col - 1], 1);
+                            newMap[row, col] = swapValue(newMap[row, col], 1);
+                            newMap[row, col + 1] = swapValue(newMap[row, col + 1], 0);
+
+                            newMap[row + 1, col - 1] = swapValue(newMap[row + 1, col - 1], 0);
+                            newMap[row + 1, col] = swapValue(newMap[row + 1, col], 0);
+                            newMap[row + 1, col + 1] = swapValue(newMap[row + 1, col + 1], 0);
+                            break;
+                        case 5: // bottom left
+                            newMap[row - 1, col - 1] = swapValue(newMap[row - 1, col - 1], 0);
+                            newMap[row - 1, col] = swapValue(newMap[row - 1, col], 1);
+                            newMap[row - 1, col + 1] = swapValue(newMap[row - 1, col + 1], 0);
+
+                            newMap[row, col - 1] = swapValue(newMap[row, col - 1], 0);
+                            newMap[row, col] = swapValue(newMap[row, col], 1);
+                            newMap[row, col + 1] = swapValue(newMap[row, col + 1], 1);
+
+                            newMap[row + 1, col - 1] = swapValue(newMap[row + 1, col - 1], 0);
+                            newMap[row + 1, col] = swapValue(newMap[row + 1, col], 0);
+                            newMap[row + 1, col + 1] = swapValue(newMap[row + 1, col + 1], 0);
+                            break;
+                        case 6: // north south
+                            newMap[row - 1, col - 1] = swapValue(newMap[row - 1, col - 1], 0);
+                            newMap[row - 1, col] = swapValue(newMap[row - 1, col], 1);
+                            newMap[row - 1, col + 1] = swapValue(newMap[row - 1, col + 1], 0);
+
+                            newMap[row, col - 1] = swapValue(newMap[row, col - 1], 0);
+                            newMap[row, col] = swapValue(newMap[row, col], 1);
+                            newMap[row, col + 1] = swapValue(newMap[row, col + 1], 0);
+
+                            newMap[row + 1, col - 1] = swapValue(newMap[row + 1, col - 1], 0);
+                            newMap[row + 1, col] = swapValue(newMap[row + 1, col], 1);
+                            newMap[row + 1, col + 1] = swapValue(newMap[row + 1, col + 1], 0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        return newMap;
+    }
+
+    /**
+     * Attempts to find a good spot to place the goal by randomly moving the levels
+     * around.
+     */
+    void findGoal(World world)
+    {
+        int x, y;
+
+        World cloneWorld = world.Clone();
+        Vector2 goalStart = new Vector2(0, 0);
+        int moves = 0;
+        for (int count= 0; count < 10; count++)
+        {
+            int moveType = Random.Range(1, 3);
+            int moveDir = Random.Range(1, 4);
+            if (cloneWorld.moveMap(moveType, moveDir))
+            {
+                if (moveType == 1)
+                {
+                    switch (moveDir)
+                    {  // Goal should move in reverse direction of the maze
+                        case 1:   // north
+                            goalStart.y += 1;
+                            break;
+                        case 2:    // south
+                            goalStart.y += -1;
+                            break;
+                        case 3:    // east
+                            goalStart.x += -1;
+                            break;
+                        case 4:    // west
+                            goalStart.x += 1;
+                            break;
+                    }
+                }
+                moves++;
+            }
+            world.setGoal(goalStart);
+        }
+    }
+
+    /**
+     * Used by buildNextMap to update the values in the new map.  Basically tries
+     * to make a value that has already been set, is not changed.
+     */
+    int swapValue(int startValue, int endValue)
+    {
+        if ((startValue == 1) || (startValue == 0))
+            return startValue;
+        else
+            return endValue;
+    }
 }
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    int[,] topLevel;
+    int[,] midLevel;
+    int[,] botLevel;
+
+    Vector2 topOffset;
+    Vector2 midOffset;
+    Vector2 botOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public World Clone()
+    {
+        World tmp = new World();
+        tmp.setTopLevel((int[,])topLevel.Clone());
+        tmp.setMidLevel((int[,])midLevel.Clone());
+        tmp.setBotLevel((int[,])botLevel.Clone());
+
+        tmp.setTopOffset(new Vector2(topOffset.x, topOffset.y));
+        tmp.setMidOffset(new Vector2(midOffset.x, midOffset.y));
+        tmp.setBotOffset(new Vector2(botOffset.x, botOffset.y));
+
+        return tmp;
+    }
+
+    public void setTopLevel(int[,] map)
+    {
+        topLevel = map;
+    }
+
+    public void setTopOffset(Vector2 offset)
+    {
+        topOffset = offset;
+    }
+
+    public void setMidLevel(int[,] map)
+    {
+        midLevel = map;
+    }
+
+    public void setMidOffset(Vector2 offset)
+    {
+        midOffset = offset;
+    }
+
+    public void setBotLevel(int[,] map)
+    {
+        botLevel = map;
+    }
+
+    public void setBotOffset(Vector2 offset)
+    {
+        botOffset = offset;
+    }
+
+    public void setGoal(Vector2 loc)
+    {
+        int row = (int)loc.y;
+        int col = (int)loc.x;
+        topLevel[row,col] = 2;
+    }
+
+    /**
+     * Drops out the bottom level, and moves the mid to bot and top to mid.
+     */
+    void cycleLevels()
+    {
+        botLevel = midLevel;
+        midLevel = topLevel;
+        topLevel = null;
+    }
+
+    /**
+     * Tries to move the selected maze in the selected direction.
+     * Returns true if successful, false otherwise.
+     */
+    public bool moveMap(int map, int direction)
+    {
+        int changeX = 0;
+        int changeY = 0;
+        switch (direction)
+        {
+            case 1:   // north
+                changeY = -1;
+                break;
+            case 2:    // south
+                changeY = 1;
+                break;
+            case 3:    // east
+                changeX = 1;
+                break;
+            case 4:    // west
+                changeX = -1;
+                break;
+
+        }
+        switch(map)
+        {
+            case 1:          // Move Top
+                topOffset.x += changeX;
+                topOffset.y += changeY;
+                break;
+            case 2:          // Move Middle
+                midOffset.x += changeX;
+                midOffset.y += changeY;
+                break;
+            case 3:          // Move Bottom
+                botOffset.x += changeX;
+                botOffset.y += changeY;
+                break;
+        }
+
+        bool overlap = false;
+        if (checkForOverlap(topLevel,topOffset,midLevel,midOffset))
+        {
+            overlap = true;
+        }
+        else if (checkForOverlap(topLevel, topOffset, botLevel, botOffset))
+        {
+            overlap = true;
+        }
+        else if (checkForOverlap(midLevel, midOffset, botLevel, botOffset))
+        {
+            overlap = true;
+        }
+
+        if (overlap)
+        {
+            switch (map)
+            {
+                case 1:          // Move Top
+                    topOffset.x -= changeX;
+                    topOffset.y -= changeY;
+                    break;
+                case 2:          // Move Middle
+                    midOffset.x -= changeX;
+                    midOffset.y -= changeY;
+                    break;
+                case 3:          // Move Bottom
+                    botOffset.x -= changeX;
+                    botOffset.y -= changeY;
+                    break;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    bool checkForOverlap(int[,] map1, Vector2 offset1, int[,] map2, Vector2 offset2)
+    {
+        int startRow = (int)(map1.GetLength(1) / 2 - offset1.y + offset2.y - map2.GetLength(1) / 2);
+        int startCol = (int)(map1.GetLength(0) / 2 - offset1.x + offset2.x - map2.GetLength(0) / 2);
+        int locX = 0;
+        for (int row = startRow; row < map1.GetLength(1); row++)
+        {
+            int locY = 0;
+            for (int col = startCol; col < map1.GetLength(0); col++)
+            {
+                if (row > 0 && col > 0)
+                {
+                    if (locX < map2.GetLength(0) && locY < map2.GetLength(1))
+                    {
+                        if ((map2[locX, locY] == 1) && (map1[row, col] == 1))
+                            return false;
+                    }
+                }
+                locY++;
+            }
+            locX++;
+        }
+
+        return false;
+    }
+}
