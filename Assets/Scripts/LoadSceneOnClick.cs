eea0209de314c949a0ac6b1dc49cf3e76761b4eb
Evan Schoenberger <evanjschoenberger@gmail.com>
2017-10-06 18:48:44 -0400

    Added Credits Screen, updated buttons accordingly

--- Assets/Scripts/LoadSceneOnClick.cs
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -15,15 +15,20 @@ public class LoadSceneOnClick : MonoBehaviour {
 		if (name == "Infinity Mode Button") {
 			SceneManager.LoadScene ("Infinity Mode");
 		}
-		/*if (gameObject.name == "Adventure Mode button") {
-			SceneManager.LoadScene ("Adventure Mode");
+		if (name == "Credits") {
+			SceneManager.LoadScene ("Credits");
 		}
+		/*
 		if (gameObject.name == "Adventure Mode button") {
 			SceneManager.LoadScene ("Adventure Mode");
 		}*/
 		if (name == "Exit") {
 			Debug.Log ("Die, Die, Die!!!!");
 			Application.Quit ();
+		}
+
+		if (name == "Back Button") {
+			SceneManager.LoadScene ("TitleScreen");
 		}
 	}
 }
