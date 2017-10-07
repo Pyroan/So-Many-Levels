a8045b909379d49afabf345cac53d34959e2c2b4
Unknown <nathanielmanning16@gmail.com>
2017-10-07 18:26:57 -0400

    Fixed resolution, scaling, and fullscreen
    can no longer drag side of window to resize, fullscreen button doesn't cut off text, and added a fullscreen toggle in options

---
+++ Assets/Scripts/FullScreenToggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullScreenToggle : MonoBehaviour {
+
+	public Toggle toggle;
+	void Start()
+	{
+		toggle = GameObject.Find("FullScreen").GetComponent<Toggle>();
+		if (Screen.fullScreen) {
+			toggle.isOn = true;
+		}
+	}
+	public void checkOn()
+	{
+		if (toggle.isOn) {
+			Screen.fullScreen = true;
+		} else {
+			Screen.fullScreen = false;
+		}
+	}
+}
